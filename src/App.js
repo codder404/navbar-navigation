9210d9fd7cbc3b3650aab332dbb010b8c82c62dc
Fernando dos Santos <nando4803@gmail.com>
2020-09-15 00:54:30 +0100

    🚀this commit add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Routes from './routes';
+
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
