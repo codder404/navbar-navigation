284f482bae0dc4ccaa4c7a321c92f4ef8353ad63
Fernando dos Santos <nando4803@gmail.com>
2020-09-15 00:50:35 +0100

    🚀this commit add route.js

---
+++ routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function Routes () {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component="" />
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes;
