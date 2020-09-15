27e5369ffc0fa3b38e6c439bf36a27035ef767bf
Fernando dos Santos <nando4803@gmail.com>
2020-09-15 02:03:52 +0100

    🚀this commit update routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
 import Navbar from './components/Navbar';
 
 function Routes () {
   return (
     <Router>
+      <Navbar />
       <Switch>
-        <Route path="/" component={Navbar} />
+        <Route path="/" exact component={Home} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/reports" component={Reports} />
       </Switch>
     </Router>
   )
