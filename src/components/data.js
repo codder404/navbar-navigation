08ff03ab0fe45c253765c7f45c23eba0d341b330
Fernando dos Santos <nando4803@gmail.com>
2020-09-15 01:09:13 +0100

    🚀this commit add sidebar data

---
+++ src/components/data.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const sidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: 'AiIcons.AiFillHome',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Home',
+    path: '/',
+    icon: 'AiIcons.AiFillHome',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Home',
+    path: '/',
+    icon: 'AiIcons.AiFillHome',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Home',
+    path: '/',
+    icon: 'AiIcons.AiFillHome',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Home',
+    path: '/',
+    icon: 'AiIcons.AiFillHome',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Home',
+    path: '/',
+    icon: 'AiIcons.AiFillHome',
+    cName: 'nav-text'
+  },
+];
