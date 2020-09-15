20af8f53c6288d471c173927be8afd19f3690c31
Fernando dos Santos <nando4803@gmail.com>
2020-09-15 01:04:11 +0100

    🚀this commit update navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 
 function Navbar () {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <>
-      <nav className="navbar">
+      <div className="navbar">
         <Link to="/" className="menu-bars">
-          <FaIcons.FaBars />
+          <FaIcons.FaBars onClick={showSidebar} />
         </Link>
+      </div>
+
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className="nav-menu-items">
+          <li className="nav-toggle">
+            <Link to="#" className="manu-bars">
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+        </ul>
       </nav>
     </>
   )
