c07bd2906f927908aebe6fa9d76036b8be9c8a0d
Fernando dos Santos <nando4803@gmail.com>
2020-09-15 01:30:22 +0100

    🚀this commit fix sidebarData icons

--- src/components/data.js
+++ src/components/data.js
@@ -4,49 +4,47 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 
-const SidebarData = [
+ export const SidebarData = [
   {
     id: 1,
     title: 'Home',
     path: '/',
-    icon: 'AiIcons.AiFillHome',
+    icon:  <AiIcons.AiFillHome />,
     cName: 'nav-text',
   },
   {
     id: 2,
     title: 'Reports',
     path: '/reports',
-    icon: 'IoIcons.IoIosPaper',
+    icon: <IoIcons.IoIosPaper />,
     cName: 'nav-text',
   },
   {
     id: 3,
     title: 'Products',
     path: '/products',
-    icon: 'FaIcons.FaCartPlus',
+    icon: <FaIcons.FaCartPlus />,
     cName: 'nav-text',
   },
   {
     id: 4,
     title: 'Team',
     path: '/team',
-    icon: 'IoIcons.IoMdPeople',
+    icon: <IoIcons.IoMdPeople />,
     cName: 'nav-text',
   },
   {
     id: 5,
     title: 'Message',
     path: '/message',
-    icon: 'FaIcons.FaEnvelopeOpenText',
+    icon: <FaIcons.FaEnvelopeOpenText />,
     cName: 'nav-text',
   },
   {
     id: 6,
     title: 'Support',
     path: '/support',
-    icon: 'IoIcons.IoMdHelpCircle',
+    icon: <IoIcons.IoMdHelpCircle />,
     cName: 'nav-text',
   }
 ];
-
-export default SidebarData;
