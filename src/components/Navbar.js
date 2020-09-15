8d37787ce4fe202a42b80bec177924286b364593
Fernando dos Santos <nando4803@gmail.com>
2020-09-15 01:54:33 +0100

    🚀this commit add iconContext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import { IconContext } from 'react-icons';
 
 import { SidebarData } from './data';
 import './styles.css';
@@ -13,31 +14,33 @@ function Navbar () {
 
   return (
     <>
-      <div className="navbar">
-        <Link to="/" className="menu-bars">
-          <FaIcons.FaBars onClick={showSidebar} />
-        </Link>
-      </div>
+      <IconContext.Provider value={{ color: '#FFF'}}>
+        <div className="navbar">
+          <Link to="/" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
 
-      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <ul className="nav-menu-items">
-          <li className="nav-toggle">
-            <Link to="#" className="menu-bars">
-              <AiIcons.AiOutlineClose />
-            </Link>
-          </li>
-          {SidebarData.map(({ id, path, title, icon, cName }) => {
-            return (
-              <li key={id} className={cName}>
-                <Link to={path}>
-                  {icon}
-                  <span>{title}</span>
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className="nav-menu-items">
+            <li className="nav-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map(({ id, path, title, icon, cName }) => {
+              return (
+                <li key={id} className={cName}>
+                  <Link to={path}>
+                    {icon}
+                    <span>{title}</span>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
     </>
   )
 }
